ab776e4ae338885eea55555482f3893f8f6b48ff
420MemesxD <55286185+420MemesxD@users.noreply.github.com>
2019-10-16 15:37:42 -0400

    Update AverageMarks.java

--- src/AverageMarks.java
+++ src/AverageMarks.java
@@ -4,265 +4,265 @@ import java.util.Scanner;
 public class AverageMarks {
 
 
+
+
     public static void main(String[] args) {
+
         Scanner keyboard = new Scanner(System.in); //creates the scanner.
         System.out.printf("please input a grade ");
-        int n1 = keyboard.nextInt();
+        int grade1 = keyboard.nextInt();
 
-        if (n1 < 0 || n1 > 100) {
+        if (grade1 < 0 || grade1 > 100) {
             System.out.printf("That grade is invalid please restart and try again");
             System.exit(0);
 
         }
         System.out.printf("please input a grade ");
-        int n2 = keyboard.nextInt();
+        int grade2 = keyboard.nextInt();
 
-        if (n2 < 0 || n2 > 100) {
+        if (grade2 < 0 || grade2 > 100) {
             System.out.printf("That grade is invalid please restart and try again");
             System.exit(0);
         }
 
         System.out.printf("please input a grade ");
-        int n3 = keyboard.nextInt();
+        int grade3 = keyboard.nextInt();
 
-        if (n3 < 0 || n3 > 100) {
+        if (grade3 < 0 || grade3 > 100) {
             System.out.printf("That grade is invalid please restart and try again");
             System.exit(0);
         }
 
         System.out.printf("please input a grade ");
-        int n4 = keyboard.nextInt();
+        int grade4 = keyboard.nextInt();
 
-        if (n4 < 0 || n4 > 100) {
+        if (grade4 < 0 || grade4 > 100) {
             System.out.printf("That grade is invalid please restart and try again");
             System.exit(0);
         }
 
         System.out.printf("please input a grade ");
-        int n5 = keyboard.nextInt();
+        int grade5 = keyboard.nextInt();
 
-        if (n5 < 0 || n5 > 100) {
+        if (grade5 < 0 || grade5 > 100) {
             System.out.printf("That grade is invalid please restart and try again");
             System.exit(0);
         }
 
         System.out.printf("please input a grade ");
-        int n6 = keyboard.nextInt();
+        int grade6 = keyboard.nextInt();
 
-        if (n6 < 0 || n6 > 100) {
+        if (grade6 < 0 || grade6 > 100) {
             System.out.printf("That grade is invalid please restart and try again");
             System.exit(0);
 
-            if (n6 < 50) {
-
-            }
         }
 
         System.out.printf("Your grades are %n");
         //Check grade level for number 1
-        if (n1 < 50) {
-            System.out.printf("1. " + n1 + " =" + " E%n");
+        if (grade1 < 50) {
+            System.out.printf("6. %d%% = E%n" ,grade1);
         }
-        if (n1 >= 50 && n1 <= 54) {
-            System.out.printf("1. " + n1 + " =" + " D%n");
+        if (grade1 >= 50 && grade1 <= 54) {
+            System.out.printf("6. %d%% = D%n" ,grade1);
         }
-        if (n1 >= 55 && n1 <= 59) {
-            System.out.printf("1. " + n1 + " =" + " D+%n");
+        if (grade1 >= 55 && grade1 <= 59) {
+            System.out.printf("6. %d%% = D+%n" ,grade1);
         }
-        if (n1 >= 60 && n1 <= 64) {
-            System.out.printf("1. " + n1 + " =" + " c%n");
+        if (grade1 >= 60 && grade1 <= 64) {
+            System.out.printf("6. %d%% = C%n" ,grade1);
         }
-        if (n1 >= 65 && n1 <= 69) {
-            System.out.printf("1. " + n1 + " =" + " C+%n");
+        if (grade1 >= 65 && grade1 <= 69) {
+            System.out.printf("6. %d%% = C+%n" ,grade1);
         }
-        if (n1 >= 70 && n1 <= 74) {
-            System.out.printf("1. " + n1 + " =" + " B%n");
+        if (grade1 >= 70 && grade1 <= 74) {
+            System.out.printf("6. %d%% = B%n" ,grade1);
         }
-        if (n1 >= 75 && n1 <= 79) {
-            System.out.printf("1. " + n1 + " =" + " B+%n");
+        if (grade1 >= 75 && grade1 <= 79) {
+            System.out.printf("6. %d%% = B+%n" ,grade1);
         }
-        if (n1 >= 80 && n1 <= 89) {
-            System.out.printf("1. " + n1 + " =" + " A%n");
+        if (grade1 >= 80 && grade1 <= 89) {
+            System.out.printf("6. %d%% = A%n" ,grade1);
         }
-        if (n1 >= 90 && n1 <= 100) {
-            System.out.printf("1. " + n1 + " =" + " A+%n");
+        if (grade1 >= 90 && grade1 <= 100) {
+            System.out.printf("6. %d%% = A+%n" ,grade1);
         }
 
         //Check grade level for number 2
-        if (n2 < 50) {
-            System.out.printf("2. " + n2 + " =" + " E%n");
+        if (grade2 < 50) {
+            System.out.printf("6. %d%% = E%n" ,grade2);
         }
-        if (n2 >= 50 && n2 <= 54) {
-            System.out.printf("2. " + n2 + " =" + " D%n");
+        if (grade2 >= 50 && grade2 <= 54) {
+            System.out.printf("6. %d%% = D%n" ,grade2);
         }
-        if (n2 >= 55 && n2 <= 59) {
-            System.out.printf("2. " + n2 + " =" + " D+%n");
+        if (grade2 >= 55 && grade2 <= 59) {
+            System.out.printf("6. %d%% = D+%n" ,grade2);
         }
-        if (n2 >= 60 && n2 <= 64) {
-            System.out.printf("2. " + n2 + " =" + " c%n");
+        if (grade2 >= 60 && grade2 <= 64) {
+            System.out.printf("6. %d%% = C%n" ,grade2);
         }
-        if (n2 >= 65 && n2 <= 69) {
-            System.out.printf("2. " + n2 + " =" + " C+%n");
+        if (grade2 >= 65 && grade2 <= 69) {
+            System.out.printf("6. %d%% = C+%n" ,grade2);
         }
-        if (n2 >= 70 && n2 <= 74) {
-            System.out.printf("2. " + n2 + " =" + " B%n");
+        if (grade2 >= 70 && grade2 <= 74) {
+            System.out.printf("6. %d%% = B%n" ,grade2);
         }
-        if (n2 >= 75 && n2 <= 79) {
-            System.out.printf("2. " + n2 + " =" + " B+%n");
+        if (grade2 >= 75 && grade2 <= 79) {
+            System.out.printf("6. %d%% = B+%n" ,grade2);
         }
-        if (n2 >= 80 && n2 <= 89) {
-            System.out.printf("2. " + n2 + " =" + " A%n");
+        if (grade2 >= 80 && grade2 <= 89) {
+            System.out.printf("6. %d%% = A%n" ,grade2);
         }
-        if (n2 >= 90 && n2 <= 100) {
-            System.out.printf("2. " + n2 + " =" + " A+%n");
+        if (grade2 >= 90 && grade2 <= 100) {
+            System.out.printf("6. %d%% = A+%n" ,grade2);
         }
 
         //Check grade level for number 3
 
-        if (n3 < 50) {
-            System.out.printf("3. " + n3 + " =" + " E%n");
+        if (grade3 < 50) {
+            System.out.printf("6. %d%% = E%n" ,grade3);
         }
-        if (n3 >= 50 && n3 <= 54) {
-            System.out.printf("3. " + n3 + " =" + " D%n");
+        if (grade3 >= 50 && grade3 <= 54) {
+            System.out.printf("6. %d%% = D%n" ,grade3);
         }
-        if (n3 >= 55 && n3 <= 59) {
-            System.out.printf("3. " + n3 + " =" + " D+%n");
+        if (grade3 >= 55 && grade3 <= 59) {
+            System.out.printf("6. %d%% = D+%n" ,grade3);
         }
-        if (n3 >= 60 && n3 <= 64) {
-            System.out.printf("3. " + n3 + " =" + " c%n");
+        if (grade3 >= 60 && grade3 <= 64) {
+            System.out.printf("6. %d%% = C%n" ,grade3);
         }
-        if (n3 >= 65 && n3 <= 69) {
-            System.out.printf("3. " + n3 + " =" + " C+%n");
+        if (grade3 >= 65 && grade3 <= 69) {
+            System.out.printf("6. %d%% = C+%n" ,grade3);
         }
-        if (n3 >= 70 && n3 <= 74) {
-            System.out.printf("3. " + n3 + " =" + " B%n");
+        if (grade3 >= 70 && grade3 <= 74) {
+            System.out.printf("6. %d%% = B%n" ,grade3);
         }
-        if (n3 >= 75 && n3 <= 79) {
-            System.out.printf("3. " + n3 + " =" + " B+%n");
+        if (grade3 >= 75 && grade3 <= 79) {
+            System.out.printf("6. %d%% = B+%n" ,grade3);
         }
-        if (n3 >= 80 && n3 <= 89) {
-            System.out.printf("3. " + n3 + " =" + " A%n");
+        if (grade3 >= 80 && grade3 <= 89) {
+            System.out.printf("6. %d%% = A%n" ,grade3);
         }
-        if (n3 >= 90 && n3 <= 300) {
-            System.out.printf("3. " + n3 + " =" + " A+%n");
+        if (grade3 >= 90 && grade3 <= 100) {
+            System.out.printf("6. %d%% = A+%n" ,grade3);
         }
 
         //Check grade level for number 4
-
-        if (n4 < 50) {
-            System.out.printf("4. " + n4 + " =" + " E%n");
+        
+        if (grade4 < 50) {
+            System.out.printf("6. %d%% = E%n" ,grade4);
         }
-        if (n4 >= 50 && n4 <= 54) {
-            System.out.printf("4. " + n4 + " =" + " D%n");
+        if (grade4 >= 50 && grade4 <= 54) {
+            System.out.printf("6. %d%% = D%n" ,grade4);
         }
-        if (n4 >= 55 && n4 <= 59) {
-            System.out.printf("4. " + n4 + " =" + " D+%n");
+        if (grade4 >= 55 && grade4 <= 59) {
+            System.out.printf("6. %d%% = D+%n" ,grade4);
         }
-        if (n4 >= 60 && n4 <= 64) {
-            System.out.printf("4. " + n4 + " =" + " c%n");
+        if (grade4 >= 60 && grade4 <= 64) {
+            System.out.printf("6. %d%% = C%n" ,grade4);
         }
-        if (n4 >= 65 && n4 <= 69) {
-            System.out.printf("4. " + n4 + " =" + " C+%n");
+        if (grade4 >= 65 && grade4 <= 69) {
+            System.out.printf("6. %d%% = C+%n" ,grade4);
         }
-        if (n4 >= 70 && n4 <= 74) {
-            System.out.printf("4. " + n4 + " =" + " B%n");
+        if (grade4 >= 70 && grade4 <= 74) {
+            System.out.printf("6. %d%% = B%n" ,grade4);
         }
-        if (n4 >= 75 && n4 <= 79) {
-            System.out.printf("4. " + n4 + " =" + " B+%n");
+        if (grade4 >= 75 && grade4 <= 79) {
+            System.out.printf("6. %d%% = B+%n" ,grade4);
         }
-        if (n4 >= 80 && n4 <= 89) {
-            System.out.printf("4. " + n4 + " =" + " A%n");
+        if (grade4 >= 80 && grade4 <= 89) {
+            System.out.printf("6. %d%% = A%n" ,grade4);
         }
-        if (n4 >= 90 && n4 <= 100) {
-            System.out.printf("4. " + n4 + " =" + " A+%n");
+        if (grade4 >= 90 && grade4 <= 100) {
+            System.out.printf("6. %d%% = A+%n" ,grade4);
         }
 
         //Check grade level for number 5
-
-        if (n5 < 50) {
-            System.out.printf("5. " + n5 + " =" + " E%n");
+        
+        if (grade5 < 50) {
+            System.out.printf("6. %d%% = E%n" ,grade5);
         }
-        if (n5 >= 50 && n5 <= 54) {
-            System.out.printf("5. " + n5 + " =" + " D%n");
+        if (grade5 >= 50 && grade5 <= 54) {
+            System.out.printf("6. %d%% = D%n" ,grade5);
         }
-        if (n5 >= 55 && n5 <= 59) {
-            System.out.printf("5. " + n5 + " =" + " D+%n");
+        if (grade5 >= 55 && grade5 <= 59) {
+            System.out.printf("6. %d%% = D+%n" ,grade5);
         }
-        if (n5 >= 60 && n5 <= 64) {
-            System.out.printf("5. " + n5 + " =" + " c%n");
+        if (grade5 >= 60 && grade5 <= 64) {
+            System.out.printf("6. %d%% = C%n" ,grade5);
         }
-        if (n5 >= 65 && n5 <= 69) {
-            System.out.printf("5. " + n5 + " =" + " C+%n");
+        if (grade5 >= 65 && grade5 <= 69) {
+            System.out.printf("6. %d%% = C+%n" ,grade5);
         }
-        if (n5 >= 70 && n5 <= 74) {
-            System.out.printf("5. " + n5 + " =" + " B%n");
+        if (grade5 >= 70 && grade5 <= 74) {
+            System.out.printf("6. %d%% = B%n" ,grade5);
         }
-        if (n5 >= 75 && n5 <= 79) {
-            System.out.printf("5. " + n5 + " =" + " B+%n");
+        if (grade5 >= 75 && grade5 <= 79) {
+            System.out.printf("6. %d%% = B+%n" ,grade5);
         }
-        if (n5 >= 80 && n5 <= 89) {
-            System.out.printf("5. " + n5 + " =" + " A%n");
+        if (grade5 >= 80 && grade5 <= 89) {
+            System.out.printf("6. %d%% = A%n" ,grade5);
         }
-        if (n5 >= 90 && n5 <= 100) {
-            System.out.printf("5. " + n5 + " =" + " A+%n");
+        if (grade5 >= 90 && grade5 <= 100) {
+            System.out.printf("6. %d%% = A+%n" ,grade5);
         }
 
         //Check grade level for number 6
-        if (n6 < 50) {
-            System.out.printf("6. " + n6 + " =" + " E%n");
+        if (grade6 < 50) {
+            System.out.printf("6. %d%% = E%n" ,grade6);
         }
-        if (n6 >= 50 && n6 <= 54) {
-            System.out.printf("6. " + n6 + " =" + " D%n");
+        if (grade6 >= 50 && grade6 <= 54) {
+            System.out.printf("6. %d%% = D%n" ,grade6);
         }
-        if (n6 >= 55 && n6 <= 59) {
-            System.out.printf("6. " + n6 + " =" + " D+%n");
+        if (grade6 >= 55 && grade6 <= 59) {
+            System.out.printf("6. %d%% = D+%n" ,grade6);
         }
-        if (n6 >= 60 && n6 <= 64) {
-            System.out.printf("6. " + n6 + " =" + " c%n");
+        if (grade6 >= 60 && grade6 <= 64) {
+            System.out.printf("6. %d%% = C%n" ,grade6);
         }
-        if (n6 >= 65 && n6 <= 69) {
-            System.out.printf("6. " + n6 + " =" + " C+%n");
+        if (grade6 >= 65 && grade6 <= 69) {
+            System.out.printf("6. %d%% = C+%n" ,grade6);
         }
-        if (n6 >= 70 && n6 <= 74) {
-            System.out.printf("6. " + n6 + " =" + " B%n");
+        if (grade6 >= 70 && grade6 <= 74) {
+            System.out.printf("6. %d%% = B%n" ,grade6);
         }
-        if (n6 >= 75 && n6 <= 79) {
-            System.out.printf("6. " + n6 + " =" + " B+%n");
+        if (grade6 >= 75 && grade6 <= 79) {
+            System.out.printf("6. %d%% = B+%n" ,grade6);
         }
-        if (n6 >= 80 && n6 <= 89) {
-            System.out.printf("6. " + n6 + " =" + " A%n");
+        if (grade6 >= 80 && grade6 <= 89) {
+            System.out.printf("6. %d%% = A%n" ,grade6);
         }
-        if (n6 >= 90 && n6 <= 100) {
-            System.out.printf("6. " + n6 + " =" + " A+%n");
+        if (grade6 >= 90 && grade6 <= 100) {
+            System.out.printf("6. %d%% = A+%n" ,grade6);
         }
 
 
-        System.out.printf("and your average grade is %.1f%%", avgrade(n1, n2, n3, n4, n5, n6));
-        if (avgrade(n1, n2, n3, n4, n5, n6) < 50) {
+        System.out.printf("and your average grade is %.1f%%", avgrade(grade1, grade2, grade3, grade4, grade5, grade6));
+        if (avgrade(grade1, grade2, grade3, grade4, grade5, grade6) < 50) {
             System.out.printf(" Thats a E%n");
         }
-        if (avgrade(n1, n2, n3, n4, n5, n6) >= 50 && avgrade(n1, n2, n3, n4, n5, n6) <= 54) {
+        if (avgrade(grade1, grade2, grade3, grade4, grade5, grade6) >= 50 && avgrade(grade1, grade2, grade3, grade4, grade5, grade6) <= 54) {
             System.out.printf(" Thats a D%n");
         }
-        if (avgrade(n1, n2, n3, n4, n5, n6) >= 55 && avgrade(n1, n2, n3, n4, n5, n6) <= 59) {
+        if (avgrade(grade1, grade2, grade3, grade4, grade5, grade6) >= 55 && avgrade(grade1, grade2, grade3, grade4, grade5, grade6) <= 59) {
             System.out.printf(" Thats a D+%n");
         }
-        if (avgrade(n1, n2, n3, n4, n5, n6) >= 60 && avgrade(n1, n2, n3, n4, n5, n6) <= 64) {
+        if (avgrade(grade1, grade2, grade3, grade4, grade5, grade6) >= 60 && avgrade(grade1, grade2, grade3, grade4, grade5, grade6) <= 64) {
             System.out.printf(" Thats a C%n");
         }
-        if (avgrade(n1, n2, n3, n4, n5, n6) >= 65 && avgrade(n1, n2, n3, n4, n5, n6) <= 69) {
+        if (avgrade(grade1, grade2, grade3, grade4, grade5, grade6) >= 65 && avgrade(grade1, grade2, grade3, grade4, grade5, grade6) <= 69) {
             System.out.printf(" Thats a C+%n");
         }
-        if (avgrade(n1, n2, n3, n4, n5, n6) >= 70 && avgrade(n1, n2, n3, n4, n5, n6) <= 74) {
+        if (avgrade(grade1, grade2, grade3, grade4, grade5, grade6) >= 70 && avgrade(grade1, grade2, grade3, grade4, grade5, grade6) <= 74) {
             System.out.printf(" Thats a B%n");
         }
-        if (avgrade(n1, n2, n3, n4, n5, n6) >= 75 && avgrade(n1, n2, n3, n4, n5, n6) <= 79) {
+        if (avgrade(grade1, grade2, grade3, grade4, grade5, grade6) >= 75 && avgrade(grade1, grade2, grade3, grade4, grade5, grade6) <= 79) {
             System.out.printf(" Thats a B+%n");
         }
-        if (avgrade(n1, n2, n3, n4, n5, n6) >= 80 && avgrade(n1, n2, n3, n4, n5, n6) <= 89) {
+        if (avgrade(grade1, grade2, grade3, grade4, grade5, grade6) >= 80 && avgrade(grade1, grade2, grade3, grade4, grade5, grade6) <= 89) {
             System.out.printf(" Thats a A%n");
         }
-        if (avgrade(n1, n2, n3, n4, n5, n6) >= 90 && avgrade(n1, n2, n3, n4, n5, n6) <= 100) {
+        if (avgrade(grade1, grade2, grade3, grade4, grade5, grade6) >= 90 && avgrade(grade1, grade2, grade3, grade4, grade5, grade6) <= 100) {
             System.out.printf(" Thats a A+%n");
         }
 
@@ -273,6 +273,7 @@ public class AverageMarks {
 
 
     }
+
 
 
 }
